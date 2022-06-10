2722ba5ef78f9c4734136793730142d1f69c2bbe
pariyashu <parihumagain@gmail.com>
2022-06-10 20:58:32 +0200

    camera bound and floow player code update

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,17 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
+public Transform target;
+public Transform leftBounds;
+public Transform rightBounds;
+private float camWidth, camHeight, levelMinX, levelMaxX;
 {
-    // Start is called before the first frame update
+    // set up camera bounds 
     void Start()
     {
-        
+        camHeight = Camera.main.orthographicSize * 2;
+        camWidth = camHeight * Camera.main.aspect;
+        float leftBoundWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        float rightBoundWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        levelMinX = leftBounds.position.x + leftBoundWidth + camWidth / 2;
+        levelMaxX = rightBounds.position.x - rightBoundWidth - camWidth / 2;
     }
 
-    // Update is called once per frame
+    // follow player
     void Update()
-    {
-        
+    { 
+        if (target){
+            // Set camera to follow player
+            float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x));
+            float targetY = target.position.y;
+            Vector3 v = new Vector3(targetX, targetY, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, v, 0.1f);
+        }
     }
 }
