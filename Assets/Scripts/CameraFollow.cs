88e034bcc815de72c72d5b71fd700295bca01fee
pariyashu <parihumagain@gmail.com>
2022-06-11 00:08:33 +0200

    camera movement fixed

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour{
     public Transform target;
-    public Transform leftBounds;
     public Transform rightBounds;
+    public Transform leftBounds;
+    
 
     // speed of the camera
     public float smoothDampTime = 0.15f;
-    private Vector3 smoothDampvelocity = Vector3.zero;
+    private Vector3 smoothDampVelocity = Vector3.zero;
     private float camWidth, camHeight, levelMinX, levelMaxX;
     // set up camera bounds 
     void Start()
     {
         camHeight = Camera.main.orthographicSize * 2;
         camWidth = camHeight * Camera.main.aspect;
-        float leftBoundWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
-        float rightBoundWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
-        levelMinX = leftBounds.position.x + leftBoundWidth + camWidth / 2;
-        levelMaxX = rightBounds.position.x - rightBoundWidth - camWidth / 2;
+        float leftBoundsWidth = leftBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        float rightBoundsWidth = rightBounds.GetComponentInChildren<SpriteRenderer>().bounds.size.x / 2;
+        levelMinX = leftBounds.position.x + leftBoundsWidth + camWidth / 2;
+        levelMaxX = rightBounds.position.x - rightBoundsWidth - camWidth / 2;
     }
 
     // follow player
@@ -29,7 +30,7 @@ public class CameraFollow : MonoBehaviour{
             // set target so that camera stays in bounds
             float targetX = Mathf.Max(levelMinX, Mathf.Min(levelMaxX, target.position.x));
             // method that moves the camera to the target position        
-            float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampvelocity.x, smoothDampTime);
+            float x = Mathf.SmoothDamp(transform.position.x, targetX, ref smoothDampVelocity.x, smoothDampTime);
             
             transform.position = new Vector3(x, transform.position.y, transform.position.z);
         }
